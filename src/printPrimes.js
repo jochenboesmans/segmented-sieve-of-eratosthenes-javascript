8ce31a004e7bb77d0302a9c071d8a3a6f6d04dca
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-08-12 15:00:28 +0200

    regularSieveOfEratosthenes implementation.

--- src/printPrimes.js
+++ src/printPrimes.js
@@ -1,6 +1,8 @@
+import primes from "./regularSieveOfEratosthenes.js";
+
 if (process.argv.length !== 3) {
   throw new Error(`Please supply exactly one CLI argument (the amount of primes to print). Example: "yarn run print_primes 100" would print the first 100 primes.`);
 } else {
   const amount = process.argv[2];
-  console.log(amount);
+  console.log(primes(amount));
 }
---
+++ src/regularSieveOfEratosthenes.js
@@ -0,0 +1,31 @@
+/*
+Functions for generating prime numbers up to a given limit n using the sieve of
+Eratosthenes method.
+*/
+import _ from "lodash";
+
+const FIRST_PRIME = 2;
+
+/*
+Returns a list of all prime numbers up to a given limit n.
+*/
+export default function primes(n) {
+  // use "slice" to avoid mutating array
+  const potPrimes = [FIRST_PRIME].concat(potentialPrimes(FIRST_PRIME, n));
+  return sweep(potPrimes, FIRST_PRIME, n, 0);
+}
+
+function potentialPrimes(min, max) {
+  // return list of odd numbers between min and max
+  const start = (min % 2 === 0) ? min + 1 : min;
+  return _.range(start, max, 2);
+}
+
+function sweep(potPrimes, p, n) {
+  const sweptPrimes = potPrimes.filter(x =>
+    !(composites(p, n).includes(x)));
+  const newP = sweptPrimes.find(e => e > p);
+  return (newP === undefined) ? sweptPrimes : sweep(sweptPrimes, newP, n);
+}
+
+const composites = (p, n) => (p * p > n) ? [] : _.range(p * p, n, p);
