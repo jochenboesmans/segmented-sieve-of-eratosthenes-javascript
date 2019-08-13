3bb7f2afe4c3808624d6c3baf0be3a75f2cf6141
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-08-13 12:10:25 +0200

    Extended prime generation time tests.

--- test/timeTest.test.js
+++ test/timeTest.test.js
@@ -4,43 +4,34 @@ const segmentedPrimesUntil = require("../src/segmentedSieveOfEratosthenes").prim
 test("Segmented sieve is faster than regular for high limits", () => {
   const n = 10000;
 
-  const regularStart = Date.now();
-  regularPrimesUntil(n);
-  const regularFinish = Date.now();
-  const regularTime = regularFinish - regularStart;
+  const times = {
+    regular: time(regularPrimesUntil, n),
+    segmented: time(segmentedPrimesUntil, n)
+  };
 
-  const segmentedStart = Date.now();
-  segmentedPrimesUntil(n);
-  const segmentedFinish = Date.now();
-  const segmentedTime = segmentedFinish - segmentedStart;
-
-  expect(segmentedTime < regularTime).toBe(true);
+  expect(times.regular > times.segmented).toBe(true);
 });
 
 test("Segmented sieve scales better than regular", () => {
-  const n1 = 3000;
-  const n2 = 30000;
+  const n = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000];
 
-  const regularN1Start = Date.now();
-  regularPrimesUntil(n1);
-  const regularN1Finish = Date.now();
-  const regularN1Time = regularN1Finish - regularN1Start;
+  const times = n.map(x => ({
+    regular: (x > 10000) ? "N/A" : time(regularPrimesUntil, x),
+    segmented: time(segmentedPrimesUntil, x)
+  }));
 
-  const regularN2Start = Date.now();
-  regularPrimesUntil(n2);
-  const regularN2Finish = Date.now();
-  const regularN2Time = regularN2Finish - regularN2Start;
-
-  const segmentedN1Start = Date.now();
-  segmentedPrimesUntil(n1);
-  const segmentedN1Finish = Date.now();
-  const segmentedN1Time = segmentedN1Finish - segmentedN1Start;
-
-  const segmentedN2Start = Date.now();
-  segmentedPrimesUntil(n2);
-  const segmentedN2Finish = Date.now();
-  const segmentedN2Time = segmentedN2Finish - segmentedN2Start;
-
-  const expected = regularN2Time / regularN1Time > segmentedN2Time / segmentedN1Time;
-  expect(expected).toBe(true);
+  // test ratios of all valid subsequent values
+  times.forEach((t, i) => {
+    if (i - 1 >= 0 && t.regular !== "N/A" && times[i - 1].regular !== 0 && times[i - 1].segmented !== 0) {
+      console.log(t.regular / times[i - 1].regular >= t.segmented / times[i - 1].segmented);
+      expect(t.regular / times[i - 1].regular >= t.segmented / times[i - 1].segmented).toBe(true);
+    }
+  });
 });
+
+function time(functionToTime, arg) {
+  const startTime = Date.now();
+  functionToTime(arg);
+  const endTime = Date.now();
+  return endTime - startTime;
+}
