1c878fe418f3bd9066b17602d7d226a8256ec74e
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-08-14 11:59:51 +0200

    regularSieve code refactor: comments, clearer code.

--- src/regularSieveOfEratosthenes.js
+++ src/regularSieveOfEratosthenes.js
@@ -14,19 +14,28 @@ function primesUntil(n) {
   return sweep(potPrimes, FIRST_PRIME, n, 0);
 }
 
+/*
+Returns a list of odd numbers between min (inclusive) and max (inclusive)
+as potential prime numbers.
+*/
 function potentialPrimes(min, max) {
-  // return list of odd numbers between min and max
   const start = (min % 2 === 0) ? min + 1 : min;
   return range(start, max + 1, 2);
 }
 
+/*
+Returns a list of primes filtered from a list of potential primes by removing
+all composites of previous numbers in the list.
+*/
 function sweep(potPrimes, p, n) {
   const sweptPrimes = potPrimes.filter(x =>
     !(composites(p, n).includes(x)));
   const newP = sweptPrimes.find(e => e > p);
+  // recursion until p is biggest remaining prime
   return (newP === undefined) ? sweptPrimes : sweep(sweptPrimes, newP, n);
 }
 
+/* Returns a list of composites of p (exclusive) lower up to n (inclusive). */
 const composites = (p, n) => (p * p > n) ? [] : range(p * p, n + 1, p);
 
 module.exports = {
